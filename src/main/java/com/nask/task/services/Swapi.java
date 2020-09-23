7be06418b57b3c89e8352e89dcb3296ec1af63b8
Jakub Kierejsza <wolframowy@o2.pl>
2020-09-23 21:14:20 +0200

    Remove unnecessary import

--- src/main/java/com/nask/task/services/Swapi.java
+++ src/main/java/com/nask/task/services/Swapi.java
@@ -2,7 +2,6 @@ package com.nask.task.services;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import com.nask.task.models.Page;
 import com.nask.task.models.Person;
 import com.nask.task.models.Planet;
@@ -10,7 +9,6 @@ import com.nask.task.models.Starship;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
